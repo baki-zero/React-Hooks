5f45c8269b09164259430a316a8166ba5f33634a
baki-zero <bakizero815@gmail.com>
2023-04-01 21:50:51 +0900

    useEffect

--- src/index.js
+++ src/index.js
@@ -1,40 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { StrictMode } from "react";
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1"
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2"
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  if (!allTabs || !Array.isArray(allTabs)) {  //allTabs가 아니거나 배열이 아니면
-    return;
-  };
-  
-  return {
-      currentItem: allTabs[currentIndex],
-      changeItem: setCurrentIndex
-  };
-};
 
 const App = () => {
-  const {currentItem, changeItem} = useTabs(0, content);
+  const sayHello = () => console.log("hello");
+  useEffect(() => {
+    sayHello();
+  });
+  const [number, setNumber] = useState(0);
+  const [aNumber, setaNumber] = useState(0);
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-    <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setaNumber(aNumber + 1)}>{aNumber}</button>
     </div>
-  );
+  )
 };
 
 const rootElement = document.getElementById("root");
