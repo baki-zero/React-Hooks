25d206cbb80a22e7787ed9a93ff174f31e15e80d
baki-zero <bakizero815@gmail.com>
2023-04-02 20:45:34 +0900

    revise useConfirm Code

--- src/useConfirm/useConfirm.js
+++ src/useConfirm/useConfirm.js
@@ -1,12 +1,15 @@
-export const useConfirm = (message="", callback, rejection) => {
-    if(typeof callback !== "function" || typeof rejection !== fu) {  //function이 아니라면  리턴
+export const useConfirm = (message="", onConfirm, onCancel) => {
+    if(!onConfirm || typeof onConfirm !== "function" ) {  //onConfirm이 존재하지 않거나 function이 아니라면 리턴
+      return;
+    }
+    if(onCancel && typeof onCancel !== "function") {  //onCancel 존재하고 function이 아니라면 리턴
       return;
     }
     const confirmAction = () => {
       if(window.confirm(message)) {
         callback();
       } else {
-        rejection();
+        onCancel();
       }
     };
     return confirmAction;
