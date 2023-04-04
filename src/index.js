51347a8ba4121be50b0c1ca884bccd238cf03ba6
baki-zero <bakizero815@gmail.com>
2023-04-04 14:00:12 +0900

    useAxios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import useAxios from "./useAxios/useAxios.js";
 
 const App = () => {
-
+  const {loading, data, error, refetch} = useAxios({
+    url:"https://yts.mx/api/v2/list_movies.json"
+  });
+  console.log(`loading: ${loading}\ndata: ${JSON.stringify(data)}\n`)
   return (
     <div className="App" style={{ height: "1000vh" }}>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>
     </div>
   );
 };
